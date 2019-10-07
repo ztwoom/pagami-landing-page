57b617443a1d400d9ecc98a6dc9c07ebac477f0a
Ahsath <ahsath@gmail.com>
2019-10-07 00:10:43 -0500

    Updated: markup refactoring

--- src/components/ValueProposition.vue
+++ src/components/ValueProposition.vue
@@ -10,38 +10,7 @@
 						Aprovechando mapas para la gestión y crecimiento empresarial
 					</h2>
 				</div>
-				<div class="newsletter mt-8 ">
-					<h2 class="font-serif">Suscribete a nuestro newsletter</h2>
-					<p class="body-1 paragraph--text mt-1">
-						y recibe una notificación cuando la aplicación esté lista para
-						Android!
-					</p>
-					<div class="d-md-flex mt-6">
-						<v-text-field
-							class="mr-md-3"
-							append-icon="mdi-email-outline"
-							background-color="surface"
-							solo
-							color="white"
-							flat
-							placeholder="Tu correo"
-							type="email"
-							validate-on-blur
-							label="Correo"
-							depresed
-							:style="{ maxWidth: !smAndUp ? '100%' : '80%' }"
-						></v-text-field>
-						<v-btn
-							elevation="0"
-							color="primary"
-							class="text-capitalize"
-							:block="!smAndUp ? true : false"
-							large
-							height="48"
-							>Enviar</v-btn
-						>
-					</div>
-				</div>
+				<app-newsletter></app-newsletter>
 			</v-col>
 			<v-col
 				cols="12"
@@ -60,15 +29,12 @@
 
 <script>
 	import BrandIllustration from "@/assets/svg/illustration.svg";
+	import AppNewsletter from "@/components/AppNewsletter.vue";
 
 	export default {
 		components: {
-			BrandIllustration
-		},
-		computed: {
-			smAndUp() {
-				return this.$vuetify.breakpoint.smAndUp;
-			}
+			BrandIllustration,
+			AppNewsletter
 		}
 	};
 </script>
