0f88f4598e400323601dab3d4000e03d22302614
Ahsath <ahsath@gmail.com>
2019-10-07 00:09:50 -0500

    Moved: ContactIcons to component

---
+++ src/components/ContactIcons.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<v-btn icon href="mailto:pagami.net@gmail.com" :large="large">
+			<v-icon>{{ mdiEmailOutline }}</v-icon>
+		</v-btn>
+		<v-btn icon href="tel:+573134854926" :large="large">
+			<v-icon>{{ mdiPhoneOutline }}</v-icon>
+		</v-btn>
+		<v-btn icon href="https://wa.me/19414130600" :large="large">
+			<v-icon>{{ mdiWhatsapp }}</v-icon>
+		</v-btn>
+	</div>
+</template>
+
+<script>
+	import { mdiEmailOutline, mdiPhoneOutline, mdiWhatsapp } from "@mdi/js";
+	export default {
+		data() {
+			return {
+				mdiEmailOutline,
+				mdiPhoneOutline,
+				mdiWhatsapp
+			};
+		},
+		props: {
+			large: {
+				type: Boolean,
+				default: false
+			}
+		}
+	};
+</script>
