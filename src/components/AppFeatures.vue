99e04778de69da08782e1bd55f8715ad2d64621a
Ahsath <ahsath@gmail.com>
2019-10-07 00:08:56 -0500

    Added: AppFeatures

--- src/components/AppFeatures.vue
+++ src/components/AppFeatures.vue
@@ -1,5 +1,5 @@
 <template>
-	<v-container class="mt-10 pa-0" tag="section" fluid>
+	<v-container class="mt-10 pa-0" tag="section" fluid id="caracteristicas">
 		<v-row no-gutters>
 			<v-col cols="12">
 				<v-container>
@@ -43,6 +43,70 @@
 						</v-img>
 					</div>
 				</v-col>
+				<v-col cols="12" md="6">
+					<v-container class="py-5">
+						<h1 class="text-center title">
+							Catálogo manejable de productos y servicios
+						</h1>
+					</v-container>
+					<div class="feature feature-3 pt-7">
+						<v-img
+							src="@/assets/img/pagami-feature-3.png"
+							class="mx-auto"
+							eager
+							max-width="320"
+						>
+						</v-img>
+					</div>
+				</v-col>
+				<v-col cols="12" md="6">
+					<v-container class="py-5">
+						<h1 class="text-center title">
+							Registrar empresas. Parte del programa de socios
+						</h1>
+					</v-container>
+					<div class="feature feature-4 pt-7">
+						<v-img
+							src="@/assets/img/pagami-feature-4.png"
+							class="mx-auto"
+							eager
+							max-width="320"
+						>
+						</v-img>
+					</div>
+				</v-col>
+				<v-col cols="12" md="6">
+					<v-container class="py-5">
+						<h1 class="text-center title">
+							Transferencia de dinero entre cuentas Pagami
+						</h1>
+					</v-container>
+					<div class="feature feature-5 pt-7">
+						<v-img
+							src="@/assets/img/pagami-feature-5.png"
+							class="mx-auto"
+							eager
+							max-width="320"
+						>
+						</v-img>
+					</div>
+				</v-col>
+				<v-col cols="12" md="6">
+					<v-container class="py-5">
+						<h1 class="text-center title">
+							Retiro de fondos a través de PayPal
+						</h1>
+					</v-container>
+					<div class="feature feature-6 pt-7">
+						<v-img
+							src="@/assets/img/pagami-feature-6.png"
+							class="mx-auto"
+							eager
+							max-width="320"
+						>
+						</v-img>
+					</div>
+				</v-col>
 			</v-row>
 		</v-row>
 	</v-container>
@@ -58,5 +122,17 @@
 	}
 	.feature-2 {
 		background-image: url("../assets/img/bench-accounting.jpg");
+	}
+	.feature-3 {
+		background-image: url("../assets/img/shoes.jpg");
+	}
+	.feature-4 {
+		background-image: url("../assets/img/taking-photo.jpg");
+	}
+	.feature-5 {
+		background-color: #3182ce;
+	}
+	.feature-6 {
+		background-color: #2c5282;
 	}
 </style>
