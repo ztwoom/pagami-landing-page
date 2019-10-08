6b61b09d09e0005e9a2f008af1d2fd8829552263
Ahsath <ahsath@gmail.com>
2019-10-07 22:00:11 -0500

    Added: vue-cli-plugin-prerender-spa

--- src/main.js
+++ src/main.js
@@ -12,5 +12,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  mounted: () => document.dispatchEvent(new Event("x-app-rendered")),
 }).$mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -91,5 +91,14 @@ module.exports = {
         }
       ]
     }
+  },
+  pluginOptions: {
+    prerenderSpa: {
+      registry: undefined,
+      renderRoutes: ["/"],
+      useRenderEvent: true,
+      headless: true,
+      onlyProduction: true
+    }
   }
 };
