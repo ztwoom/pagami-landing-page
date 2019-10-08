ebb3a5b49d639171e6310ee7da616dc592abbdc8
Ahsath <ahsath@gmail.com>
2019-10-07 22:19:00 -0500

    Updated: listen for verified form submisions

--- src/lambda/submission-created.js
+++ src/lambda/submission-created.js
@@ -1,7 +1,7 @@
 exports.handler = (event, context, cb) => {
   console.log(event);
+  console.log(context);
   cb(null, {
-    headers: { "Access-Control-Allow-Origin": "*" },
     statusCode: 200
   });
 };
