cacfdfc1503ead3349de8a68d51ee318f41aa10b
Ahsath <ahsath@gmail.com>
2019-10-08 19:17:21 -0500

    Updated: moved to project root

---
+++ lambda/submission-created.js
@@ -0,0 +1,24 @@
+const Mailchimp = require("mailchimp-api-v3");
+const mp = new Mailchimp(process.env.MP_API_KEY);
+
+exports.handler = async (evt, context) => {
+  // let { email } = JSON.parse(evt.body).payload;
+  let payload = JSON.parse(evt.body).payload;
+
+  return {
+    body: JSON.stringify(payload),
+    statusCode: 200
+  };
+  // try {
+  //   const list = await mp.post("/lists/50afccf669/members", { email, status: "pending" });
+  //   return {
+  //     body: JSON.stringify(list),
+  //     statusCode: 200
+  //   };
+  // } catch (error) {
+  //   return {
+  //     body: error,
+  //     statusCode: 400
+  //   };
+  // }
+};
--- src/lambda/submission-created.js
+++
@@ -1,7 +0,0 @@
-exports.handler = (event, context, cb) => {
-  let payload = JSON.parse(event.body).payload;
-  console.log(payload);
-  cb(null, {
-    statusCode: 200
-  });
-};
