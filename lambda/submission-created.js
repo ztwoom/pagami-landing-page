c318297d09a946a1dabc7015dd8e6b02391095a8
Ahsath <ahsath@gmail.com>
2019-10-08 20:37:35 -0500

    t

--- lambda/submission-created.js
+++ lambda/submission-created.js
@@ -3,8 +3,8 @@ const mp = new Mailchimp(process.env.MP_API_KEY);
 
 exports.handler = async (evt, context) => {
   // let { email } = JSON.parse(evt.body).payload;
-  let data = JSON.parse(evt.body);
-  console.log("payloaddddd", data);
+  let site = JSON.parse(evt.body).site;
+  console.log("siteInfo", site);
   // return {s
   // try {
   //   const list = await mp.post("/lists/50afccf669/members", { email, status: "pending" });
